3b4f3194745a18783f8a4e8d7e72d56c78aa7bd5
Baodzvip <116901121+Baodzvip@users.noreply.github.com>
2024-11-22 01:31:33 +0700

    Update LemonKillaura.lua

--- Scripts/LemonKillaura.lua
+++ Scripts/LemonKillaura.lua
@@ -3,8 +3,8 @@ getgenv().KillAURA = true
 spawn(function()
   while getgenv().KillAURA do
     task.wait()
-       sethiddenproperty(game.Players.LocalPlayer, "SimulationRadius", 200)
-          sethiddenproperty(game.Players.LocalPlayer, "MaxSimulationRadius", 200)
+       sethiddenproperty(game.Players.LocalPlayer, "SimulationRadius", 50)
+          sethiddenproperty(game.Players.LocalPlayer, "MaxSimulationRadius", 50)
               for _, v in pairs(game:GetService("Workspace"):GetDescendants()) do
                 if v.ClassName == 'Humanoid' and v.Parent.Name ~= game.Players.LocalPlayer.Name then
              v.Health = 0
